f86bbfe906cf43e9a180e03ed543ebe6f340e5dd
Alex Francisco <alex2007f@gmail.com>
2022-01-06 16:46:57 -0300

    Resolução Ex 10

---
+++ Exercícios-Capítulo3_Métodos_de_Array/exercicio_10.js
@@ -0,0 +1,7 @@
+/* Exercício 10 - E tem alguma diferença?
+Diga, em poucas palavras, qual a diferença entre os métodos auxiliares forEach e map.
+*/
+
+/* Resposta: A diferença é que o forEach é usado quando queremos apenas iterar sobre os itens de um array, exibindo
+esses valores. O uso da map já é válido quando queremos iterar um array fazendo alguma alteração nos valores, de 
+forma que o resultado tenha a saída em um novo array, sem modificar o original */
