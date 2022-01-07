cb9116d8e693babaa5414f8b73e6deca3578421a
Alex Francisco <alex2007f@gmail.com>
2022-01-07 10:32:15 -0300

    Resolução Ex 02

---
+++ Exercícios-Capítulo4_Iteração_com_Iteradores_e_Iteráveis/exercicio_02.js
@@ -0,0 +1,13 @@
+/*
+Desenvolva a função 'isListaVazia' que recebe como parâmetro uma lista de números inteiros
+qualquer e retorna o valor true caso esta lista não tenha nenhum item e false para os demais
+resultados. A lógica deve ser feita usando somente a propriedade done do objeto que é obtido
+ao executar o next no iterador do array.
+*/
+
+function isListaVazia(listaNumInteros) {
+  return listaNumInteros[Symbol.iterator]().next().done;
+}
+
+console.log(isListaVazia([]));
+console.log(isListaVazia([1, 2]));
